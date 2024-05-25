a14e416bb5a26cccfb8b402ea9a528c1d4a3a376
mudiiiiii <otomudiaga@gmail.com>
2024-05-24 23:32:07 -0400

    menucommit

--- client/Delici/src/components/Menhome/Menusection.js
+++ client/Delici/src/components/Menhome/Menusection.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import bgone from '../../assets/images/background/bg-5.png'
 import Bgtwo from '../../assets/images/background/bg-6.png'
-import five from '../../assets/images/resource/menu-image-5.png'
-import six from '../../assets/images/resource/menu-image-6.png'
-import seven from '../../assets/images/resource/menu-image-7.png'
-import eight from '../../assets/images/resource/menu-image-8.png'
+import five from '../../assets/images/03photos/food/1Z6A0721.jpg'
+import six from '../../assets/images/03photos/food/1Z6A0702.jpg'
+import seven from '../../assets/images/03photos/food/1Z6A1069.jpg'
+import eight from '../../assets/images/03photos/food/1Z6A1029.jpg'
 import nine from '../../assets/images/resource/menu-image-9.png'
 import ten from '../../assets/images/resource/menu-image-10.png'
+import eleven from '../../assets/images/03photos/food/1Z6A1139.jpg'
+import twelve from '../../assets/images/03photos/food/1Z6A0787.jpg'
+import thirteen from '../../assets/images/03photos/food/1Z6A0862.jpg'
 
 function Menusection() {
     const [tabMenu, tabActive] = useState({ a: true });
@@ -34,9 +37,9 @@ function Menusection() {
                     <div className="tabs-box menu-tabs">
                         <div className="buttons">
                             <ul className="tab-buttons clearfix">
-                                <li className={`tab-btn ${tabMenu.a && "active-btn"}`} onClick={() => tabActive({ a: true })} data-tab="#tab-1"><span>MORNING</span></li>
-                                <li className={`tab-btn ${tabMenu.b && "active-btn"}`} onClick={() => tabActive({ b: true })} data-tab="#tab-2"><span>WEEKDAY LUNCH</span></li>
-                                <li className={`tab-btn ${tabMenu.c && "active-btn"}`} onClick={() => tabActive({ c: true })} data-tab="#tab-3"><span>DINNER</span></li>
+                                <li className={`tab-btn ${tabMenu.a && "active-btn"}`} onClick={() => tabActive({ a: true })} data-tab="#tab-1"><span>APPETIZERS</span></li>
+                                <li className={`tab-btn ${tabMenu.b && "active-btn"}`} onClick={() => tabActive({ b: true })} data-tab="#tab-2"><span>ENTREE</span></li>
+                                <li className={`tab-btn ${tabMenu.c && "active-btn"}`} onClick={() => tabActive({ c: true })} data-tab="#tab-3"><span>DESSERT</span></li>
                                 <li className={`tab-btn ${tabMenu.d && "active-btn"}`} onClick={() => tabActive({ d: true })} data-tab="#tab-4"><span>DRINKS</span></li>
                             </ul>
                         </div>
@@ -49,17 +52,18 @@ function Menusection() {
 
                                             <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={five} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon</Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={five} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">Chicken and Meat Croquette</Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">Scrumptious and cheezy sauteed vegetables with diced chicken, moulded with bread crumbs.</Link></div>
                                                 </div>
                                             </div>
 
                                             <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={six} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon </Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={six} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">Spring Rolls - Chicken, vegetable, Prawns </Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">Tender fried parcels of flaky pastry rolls made with fresh vegetables, stuffed chicken or prawns.
+Served with our special sweet chili sauce.</Link></div>
                                                 </div>
                                             </div>
 
@@ -110,25 +114,27 @@ function Menusection() {
 
                                         <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={five} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon</Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={eleven} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">Classic Burger</Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">Juicy mouth-watering beef burger: it comes with fresh bread rolls stuffed with minced meat,
+fresh lettuce and melted mozzarella cheese, Severed with French fries and chili mayo dip.</Link></div>
                                                 </div>
                                             </div>
 
                                             <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={six} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon </Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={seven} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">Savory Roast Pasta</Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">Full of flavor pasta prepared in Roasted bell pepper sauce and severed with boneless chicken</Link></div>
                                                 </div>
                                             </div>
 
                                             <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={seven} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon</Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={seven} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">Grilled Croaker Fish</Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">Well marinated croaker fish in hot and spicy pepper sauce, served with sweet potatoe fries or
+yam chips.</Link></div>
                                                 </div>
                                             </div>
                                         </div>
@@ -138,25 +144,28 @@ function Menusection() {
 
                                             <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={eight} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon </Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={eight} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">Shredded Beef In Mushroom Sauce </Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">Delicious shredded beef in creamy mushroom sauce with mixed vegetables and special spices
+served with rice options.</Link></div>
                                                 </div>
                                             </div>
 
                                             <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={nine} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon</Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={twelve} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">AbuleOja Platter</Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">A relish to share with friends. Well marinated and spicy goat meat, snails, gizzards, grilled
+prawns and fried yam served on a platter with our special chili and mayo dip.</Link></div>
                                                 </div>
                                             </div>
 
                                             <div className="dish-block">
                                                 <div className="inner-box">
-                                                    {/* <div className="dish-image"><Link to="#"><img src={ten} alt="" /></Link></div> */}
-                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">coming soon</Link></h5></div><div className="price"></div></div>
-                                                    <div className="text desc"><Link to="#">coming soon coming soon coming soon coming soon coming soon coming soon</Link></div>
+                                                    <div className="dish-image"><Link to="#"><img src={thirteen} alt="" /></Link></div>
+                                                    <div className="title clearfix"><div className="ttl clearfix"><h5><Link to="#">03 Platter (For 2)</Link></h5></div><div className="price"></div></div>
+                                                    <div className="text desc"><Link to="#">A platter to unwind and reconnect with friends. Well prepared spring rolls, samosa, chicken
+satay, wings, meat croquette, chicken croquette, and French fries.</Link></div>
                                                 </div>
                                             </div>
                                         </div>
@@ -295,6 +304,12 @@ function Menusection() {
                                 <span className="btn-wrap">
                                     <span className="text-one">view all menu</span>
                                     <span className="text-two">view all menu</span>
+                                </span>
+                            </Link>
+                            <Link to="/#" className="theme-btn btn-style-two clearfix">
+                                <span className="btn-wrap">
+                                    <span className="text-one">order now</span>
+                                    <span className="text-two">order now</span>
                                 </span>
                             </Link>
                         </div>
